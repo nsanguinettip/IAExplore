6f00c2c3fff512c9f906c2f788975418051ee160
nsanguip <nsanguinetti19@gmail.com>
2022-02-26 16:09:07 -0300

    fix

--- Setup_Deploy_AutoSpam.sh
+++ Setup_Deploy_AutoSpam.sh
@@ -3,9 +3,6 @@ export version=1.6
 export user=SETUP
 export vm_num=SETUP
 mkdir ~/Scripts
-cp ~/Downloads/Setup_Config.sh ~/Scripts/Setup_Config.sh 
-cp ~/Downloads/Setup_Repositories.sh ~/Scripts/Setup_Repositories.sh 
-cd ~/Scripts 
 chmod +x Setup_Repositories.sh Setup_Config.sh 
 ./Setup_Repositories.sh 
 ./Setup_Config.sh 

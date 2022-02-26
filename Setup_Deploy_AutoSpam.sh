38b70776601b4e38416071a351b90112546efe10
nsanguip <nsanguinetti19@gmail.com>
2022-02-26 16:46:40 -0300

    fix2

--- Setup_Deploy_AutoSpam.sh
+++ Setup_Deploy_AutoSpam.sh
@@ -1,12 +1,10 @@
 #sudo apt install git
 export version=1.6 
-export user=SETUP
-export vm_num=SETUP
 mkdir ~/Scripts
 chmod +x Setup_Repositories.sh Setup_Config.sh 
 ./Setup_Repositories.sh 
 ./Setup_Config.sh 
 cd MillosScripts 
 ./DeployAutoSpam.sh 
-cp ~/Config/JaimeConfigFiles/$vm_num.$user/config.json ~/AutoSpam/AutoSpamv$version/config.json 
+cp ~/Config/JaimeConfigFiles/$VM_NUMBER.$TW_USER/config.json ~/AutoSpam/AutoSpamv$version/config.json 
 gedit ~/AutoSpam/AutoSpamv$version/config.json

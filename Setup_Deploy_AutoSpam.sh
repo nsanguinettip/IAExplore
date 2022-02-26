bdacd24f2e5e82f276ed5367c42884560509f7e3
nsanguip <nsanguinetti19@gmail.com>
2022-02-26 16:53:08 -0300

    fix

--- Setup_Deploy_AutoSpam.sh
+++ Setup_Deploy_AutoSpam.sh
@@ -5,6 +5,7 @@ chmod +x Setup_Repositories.sh Setup_Config.sh
 ./Setup_Repositories.sh 
 ./Setup_Config.sh 
 cd MillosScripts 
+chmod +x DeployAutoSpam.sh
 ./DeployAutoSpam.sh 
 cp ~/Config/JaimeConfigFiles/$VM_NUMBER.$TW_USER/config.json ~/AutoSpam/AutoSpamv$version/config.json 
 gedit ~/AutoSpam/AutoSpamv$version/config.json
--- Setup_Global_Variables.sh
+++ Setup_Global_Variables.sh
@@ -1,3 +1,3 @@
-echo export TW_USER=test >> .bashrc
-echo export VM_NUMBER=-1 >> .bashrc
-echo export GIT_KEY=SETUP >> .bashrc
+echo export TW_USER=test >> ~/.bashrc
+echo export VM_NUMBER=-1 >> ~/.bashrc
+echo export GIT_KEY=SETUP >> ~/.bashrc
